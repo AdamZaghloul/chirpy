961b544494b037c634c3f8bba0f4636bd4a6b74e
AdamZaghloul <adam.zaghloul95@gmail.com>
2024-12-09 15:28:39 -0500

    Added chirp validations.

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
 )
@@ -23,9 +25,10 @@ func main() {
 	}
 
 	serveMux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
-	serveMux.Handle("/healthz", http.HandlerFunc(healthHandler))
-	serveMux.Handle("/metrics", http.HandlerFunc(cfg.metricsHandler))
-	serveMux.Handle("/reset", http.HandlerFunc(cfg.resetMetricsHandler))
+	serveMux.Handle("GET /api/healthz", http.HandlerFunc(healthHandler))
+	serveMux.Handle("POST /api/validate_chirp", http.HandlerFunc(validateHandler))
+	serveMux.Handle("GET /admin/metrics", http.HandlerFunc(cfg.metricsHandler))
+	serveMux.Handle("POST /admin/reset", http.HandlerFunc(cfg.resetMetricsHandler))
 
 	err := server.ListenAndServe()
 	if err != nil {
@@ -51,9 +54,15 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(200)
-	_, err := w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+	_, err := w.Write([]byte(fmt.Sprintf(`
+		<html>
+			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+		</html>`, cfg.fileserverHits.Load())))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
@@ -67,4 +76,48 @@ func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+}
+
+func validateHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	//checks here
+
+	type returnVals struct {
+		Err   string `json:"error"`
+		Valid bool   `json:"valid"`
+	}
+
+	respBody := returnVals{}
+	var code int
+
+	if len(params.Body) <= 140 {
+		respBody.Valid = true
+		code = 200
+	} else {
+		respBody.Err = "Chirp is too long"
+		code = 400
+	}
+
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
 }
